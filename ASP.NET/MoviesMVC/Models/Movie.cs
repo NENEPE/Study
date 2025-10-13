8768af390a9d28210f6562d021a25ce52578463a
NENEPE <oleksandrprystavsky@gmail.com>
2025-10-13 03:00:41 +0200

    Created "MoviesMVC" project

---
+++ ASP.NET/MoviesMVC/Models/Movie.cs
@@ -0,0 +1,13 @@
+﻿namespace Movies
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        public string? Director { get; set; }
+        public int ReleaseYear { get; set; }
+        public string? Genre { get; set; }
+        public string? Desc { get; set; }
+        public string? Poster { get; set; }
+    }
+}
